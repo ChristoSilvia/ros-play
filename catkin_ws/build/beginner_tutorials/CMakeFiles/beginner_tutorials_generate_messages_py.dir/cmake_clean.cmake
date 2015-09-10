b341ec738ef358025b240b613fd06e641e0a78bb
Christopher Silvia <christopher.silvia@gmail.com>
2015-09-10 15:27:56 -0400

    did a lot of stuff

---
+++ catkin_ws/build/beginner_tutorials/CMakeFiles/beginner_tutorials_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/beginner_tutorials_generate_messages_py"
+  "/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/msg/_Num.py"
+  "/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/srv/_AddTwoInts.py"
+  "/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/msg/__init__.py"
+  "/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/python2.7/dist-packages/beginner_tutorials/srv/__init__.py"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/beginner_tutorials_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,12 +7,6 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export CPATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/include:$CPATH"
-export LD_LIBRARY_PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
-export PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/bin:$PATH"
-export PKG_CONFIG_PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/cs4752/tmp/ros-play/catkin_ws/devel/lib:/opt/ros/indigo/lib"
+export PKG_CONFIG_PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/cs4752/tmp/ros-play/catkin_ws/build"
-export PYTHONPATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/cs4752/tmp/ros-play/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/cs4752/tmp/ros-play/catkin_ws/src:$ROS_PACKAGE_PATH"
