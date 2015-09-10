bb0a5357eab7999180b66a1c19044ef2c0caa522
CS4752 <cs4752@en-cs-lgg03-04.coecis.cornell.edu>
2015-09-10 13:49:07 -0400

    made catkin workspace

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/cs4752/tmp/ros-play/catkin_ws/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/cs4752/tmp/ros-play/catkin_ws/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/lib:/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/pkgconfig:/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/cs4752/tmp/ros-play/catkin_ws/build"
+export PYTHONPATH="/home/cs4752/tmp/ros-play/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cs4752/tmp/ros-play/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/cs4752/tmp/ros-play/catkin_ws/src:$ROS_PACKAGE_PATH"
