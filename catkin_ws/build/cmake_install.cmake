3d21fb09d34e3e08859847f71366d22db4f5a28c
CS4752 <cs4752@en-cs-lgg03-04.coecis.cornell.edu>
2015-09-10 13:52:02 -0400

    built a package

--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -125,6 +125,7 @@ ENDIF(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unsp
 IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   INCLUDE("/home/cs4752/tmp/ros-play/catkin_ws/build/gtest/cmake_install.cmake")
+  INCLUDE("/home/cs4752/tmp/ros-play/catkin_ws/build/beginner_tutorials/cmake_install.cmake")
 
 ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
 
