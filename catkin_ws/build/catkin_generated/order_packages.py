41e2a3de56f4a8e0c163e0a333c655cf4cfa0507
Christopher Silvia <christopher.silvia@gmail.com>
2015-09-10 13:58:04 -0400

    amended package.xml

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/cs4752/tmp/ros-play/catkin_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/cs4752/tmp/ros-play/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/cs4752/tmp/ros-play/catkin_ws/devel;/opt/ros/indigo" != "" else []
+underlay_workspaces = "/opt/ros/indigo".split(';') if "/opt/ros/indigo" != "" else []
